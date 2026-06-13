f2b6f9b47910812b65c3af1975477c90af56762a
mzashah <mz.ashah@outlook.com>
2026-06-13 20:05:04 +0500

    Add routes/api.php

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout',  [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::get('me',       [AuthController::class, 'me']);
+    });
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('users', UserController::class);
+});
